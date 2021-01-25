e36a70ae6843d0e610f7c9343189ae1adfa330fd
HUONG TRINH XUAN <txhuong@rd.local>
2021-01-25 07:58:04 +0700

    fix todo reducer

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -8,13 +8,13 @@ const initState = [{
 const TodoReducer = (state = initState, action) => {
     switch (action.type) {
         case 'ADD_TODO':
-            return [...state, { id: state.length == 0 ? 0 : state[state.length - 1].id + 1, text: action.payload.text, mark: false }];
+            return [...state, { id: state.length === 0 ? 0 : state[state.length - 1].id + 1, text: action.payload.text, mark: false }];
         case 'EDIT_TODO':
-            return state.map((todo) => { todo.id == action.payload.id ? action.payload : todo });
+            return state.map((todo) => todo.id === action.payload.id ? action.payload : todo);
         case 'REMOVE_TODO':
-            return state.map((todo) => { if (todo.id != action.payload.id) return todo });
+            return state.map((todo) => { if (todo.id !== action.payload.id) return todo });
         case 'STATUS_TODO':
-            return state.map((todo) => { todo.id == action.payload.id ? { id: todo.id, text: todo.text, mark: action.payload.mark } : todo });
+            return state.map((todo) => todo.id === action.payload.id ? { ...todo, mark: action.payload.mark } : todo);
         case 'STATUS_ALL':
             return state.map((todo) => { return { id: todo.id, text: todo.text, mark: action.payload.mark } });
         default:
